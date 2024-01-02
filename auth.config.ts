b4ff494c7eff6e0588d9a4fc2bbca77041aea99c
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-01-01 23:53:38 -0900

    Callbacks Done

--- auth.config.ts
+++ auth.config.ts
@@ -10,19 +10,20 @@ export default {
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
-        if(validatedFields.success) {
-          const {email, password} = validatedFields.data
 
-          const user = await getUserByEmail(email)
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
 
-          if(!user || !user.password) return null
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
 
-          const isPasswordMatch = await bcrypt.compare(password, user.password)
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-          if (isPasswordMatch) return user
+          if (passwordsMatch) return user;
         }
-        return null
-      }
-    })
+
+        return null;
+      },
+    }),
   ],
 } satisfies NextAuthConfig;
