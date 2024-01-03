3723a04cd07c2d521e9b9c6ac3965b2c11f2afae
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-01-03 10:11:01 -0900

    Reset password email done

--- routes.ts
+++ routes.ts
@@ -5,10 +5,12 @@ export const publicRoutes = [
 ]
 
 export const authRoutes = [
-    "/auth/login",
-    "/auth/register",
-    "/auth/error"
-]
+  "/auth/login",
+  "/auth/register",
+  "/auth/error",
+  "/auth/reset",
+  "/auth/new-password",
+];
 
 export const apiAuthPrefix = "/api/auth" 
 
